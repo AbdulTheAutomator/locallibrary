f4f5ba99c7226bfd107a6c4a3d860389b621b70f
The-BI-Developer <abdulwasay12@live.com>
2024-04-02 09:28:04 +0100

    bugfix:added missing urlpatterns (special var) in catalog.url.py

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -30,7 +30,7 @@ urlpatterns+=[
     path('catalog/',include('catalog.urls')),
 ] +\
 [
-    path('',RedirectView.as_view(url='catalog/',permenant=True)),
+    path('',RedirectView.as_view(url='catalog/',permanent=True)),
     ]
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
